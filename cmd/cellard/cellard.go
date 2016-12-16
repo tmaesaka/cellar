12625a0ffa97786aa4b2132c91fbe32b85d5018f
Toru Maesaka <toru@torumk.com>
2016-12-15 19:14:57 -0800

    Override the existing flag.Usage output
    Hoping to provide something friendly and unique as we progress.
    If not, we can always resort back to the out-of-box behavior.

--- cmd/cellard/cellard.go
+++ cmd/cellard/cellard.go
@@ -6,6 +6,18 @@ import (
 	"os"
 )
 
+func usage() {
+	fmt.Printf("cellard (dev)\n")
+	fmt.Printf("  --port <num>  %s\n", flag.Lookup("port").Usage)
+	fmt.Printf("  -h, --help    Print this help message\n")
+
+	os.Exit(0)
+}
+
+func init() {
+	flag.Usage = usage
+}
+
 func main() {
 	port := flag.Int("port", 8084, "TCP port number to listen on (default: 8084)")
 	flag.Parse()
