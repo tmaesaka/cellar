6f3753d58143662926e4047a3d44a186a6f5248e
Toru Maesaka <toru@torumk.com>
2016-12-17 12:07:48 -0800

    Accept -v and -vv options from the command-line
    I'm not happy with this code because I prefer representing the
    verbosity level as an integer. However, it's not worth the time
    and effort to work around the intented usage of the flag package.
    
    Resolves #5

--- api/config.go
+++ api/config.go
@@ -3,8 +3,10 @@ package api
 // config holds the server settings. These settings could come from
 // the command-line and/or a configuration file.
 type config struct {
-	Port    int    `json:"port"`    // TCP port number that the server listens on
-	DataDir string `json:"datadir"` // Path to the Cellar data directory
+	Port        int    `json:"port"`         // TCP port number that the server listens on
+	DataDir     string `json:"datadir"`      // Path to the Cellar data directory
+	Verbose     bool   `json:"verbose"`      // Server is verbose
+	VeryVerbose bool   `json:"very_verbose"` // Server is very verbose
 }
 
 func NewConfig() *config {
