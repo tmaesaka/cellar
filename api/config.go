bd27404f9dd3e9215872ef2ec1b98938a736b48d
Toru Maesaka <toru@torumk.com>
2016-12-16 23:14:35 -0800

    Accept a --datadir option from the command-line
    Resolves #3

--- api/config.go
+++ api/config.go
@@ -3,7 +3,8 @@ package api
 // config holds the server settings. These settings could come from
 // the command-line and/or a configuration file.
 type config struct {
-	Port int `json:"port"` // TCP port number that the server listens on
+	Port    int    `json:"port"`    // TCP port number that the server listens on
+	DataDir string `json:"datadir"` // Path to the Cellar data directory
 }
 
 func NewConfig() *config {
--- cmd/cellard/cellard.go
+++ cmd/cellard/cellard.go
@@ -10,8 +10,9 @@ import (
 
 func usage() {
 	fmt.Printf("cellard (dev)\n")
-	fmt.Printf("  --port <num>  %s\n", flag.Lookup("port").Usage)
-	fmt.Printf("  -h, --help    Print this help message\n")
+	fmt.Printf("  --port <num>      %s\n", flag.Lookup("port").Usage)
+	fmt.Printf("  --datadir <path>  %s\n", flag.Lookup("datadir").Usage)
+	fmt.Printf("  -h, --help        Print this help message\n")
 
 	os.Exit(0)
 }
@@ -24,6 +25,7 @@ func main() {
 	config := api.NewConfig()
 
 	flag.IntVar(&config.Port, "port", 8084, "TCP port number to listen on (default: 8084)")
+	flag.StringVar(&config.DataDir, "datadir", ".", "Path to the Cellar data directory")
 	flag.Parse()
 
 	fmt.Fprintf(os.Stderr, "Starting cellard... listening on port %d\n", config.Port)
