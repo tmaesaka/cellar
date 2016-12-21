d56e559c3c20224ef937992386dd44e10b3332b7
Toru Maesaka <toru@torumk.com>
2016-12-20 22:47:46 -0800

    Add ensureDataDirPresence() to the api package
    The Cellar daemon is powerless without its data directory.
    Therefore, this is one of the first functions that cellard calls.
    
    Related #10

--- api/server.go
+++ api/server.go
@@ -7,9 +7,23 @@ import (
 	"os"
 )
 
+func ensureDataDirPresence(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		if err = os.MkdirAll(path, 0755); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 // Run checks if the provided configuration is sufficient to run the
 // Cellar daemon. If successful, a Web API server will be started.
 func Run(config *config) error {
+	if err := ensureDataDirPresence(config.DataDir); err != nil {
+		log.Fatalf("Invalid datadir: %v", err)
+	}
+
 	fmt.Fprintf(os.Stderr, "Starting cellard... listening on port %d\n", config.Port)
 
 	addr := fmt.Sprintf(":%d", config.Port)
