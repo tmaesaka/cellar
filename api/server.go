a65727bf501b375107c499bf4f83e6b90d98bb75
Toru Maesaka <toru@torumk.com>
2016-12-30 19:37:11 -0800

    Use httprouter as our web api multiplexer

--- api/server.go
+++ api/server.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/tmaesaka/cellar/config"
 )
 
@@ -30,12 +31,14 @@ func Run(config *config.ApiConfig) error {
 		log.Fatalf("Invalid datadir: %v", err)
 	}
 
+	router := httprouter.New()
+
 	fmt.Fprintf(os.Stderr, "Starting cellard... listening on port %d\n", config.Port)
 
 	addr := fmt.Sprintf(":%d", config.Port)
 
 	// Start an empty http server until we decide on handler strategy.
-	log.Fatal(http.ListenAndServe(addr, nil))
+	log.Fatal(http.ListenAndServe(addr, router))
 
 	return nil
 }
