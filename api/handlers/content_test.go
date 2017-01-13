8cd8228bebc2cf98f2fcb85b6ddf2eb439da30fe
Toru Maesaka <toru@torumk.com>
2017-01-12 22:54:10 -0800

    Restructure the content handler test

--- api/handlers/content_test.go
+++ api/handlers/content_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	"github.com/husobee/vestigo"
+	"github.com/libgit2/git2go"
 )
 
 func TestCreateContentHandler(t *testing.T) {
@@ -19,7 +20,50 @@ func TestCreateContentHandler(t *testing.T) {
 	testFile := "test.txt"
 	reqPath := path.Join("/repos", testRepoName, "contents", testFile)
 
-	t.Run("without content", func(t *testing.T) {
+	t.Run("with valid repository", func(t *testing.T) {
+		repoPath := repoPath(testCfg.DataDir, testRepoName)
+		git.InitRepository(repoPath, true)
+
+		t.Run("with base64 encoded content", func(t *testing.T) {
+			params := url.Values{}
+			params.Set("content", "Y2VsbGFy")
+			encodedParams := strings.NewReader(params.Encode())
+
+			req, _ := http.NewRequest("POST", reqPath, encodedParams)
+			req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+			recorder := httptest.NewRecorder()
+			router.ServeHTTP(recorder, req)
+
+			if recorder.Code != http.StatusOK {
+				t.Errorf("Exepected status code 200; got %d", recorder.Code)
+			}
+		})
+
+		t.Run("with non-base64 encoded content", func(t *testing.T) {
+			params := url.Values{}
+			params.Set("content", "cellar")
+			encodedParams := strings.NewReader(params.Encode())
+
+			req, _ := http.NewRequest("POST", reqPath, encodedParams)
+			req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+			recorder := httptest.NewRecorder()
+			router.ServeHTTP(recorder, req)
+
+			if recorder.Code != http.StatusBadRequest {
+				t.Errorf("Exepected status code 400; got %d", recorder.Code)
+			}
+
+			errResp := decodeError(recorder.Body.String())
+
+			if errResp.Message != "content must be base64 encoded" {
+				t.Errorf("Mismatched error message: %s", errResp.Message)
+			}
+		})
+
+		cleanup()
+	})
+
+	t.Run("without content param", func(t *testing.T) {
 		req, _ := http.NewRequest("POST", reqPath, nil)
 		recorder := httptest.NewRecorder()
 		router.ServeHTTP(recorder, req)
@@ -32,42 +76,6 @@ func TestCreateContentHandler(t *testing.T) {
 
 		if errResp.Message != "content parameter required" {
 			t.Errorf("Mismatched error message: %s", recorder.Body.String())
-		}
-	})
-
-	t.Run("with base64 encoded content", func(t *testing.T) {
-		params := url.Values{}
-		params.Set("content", "Y2VsbGFy")
-		encodedParams := strings.NewReader(params.Encode())
-
-		req, _ := http.NewRequest("POST", reqPath, encodedParams)
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-		recorder := httptest.NewRecorder()
-		router.ServeHTTP(recorder, req)
-
-		if recorder.Code != http.StatusOK {
-			t.Errorf("Exepected status code 200; got %d", recorder.Code)
-		}
-	})
-
-	t.Run("with non-base64 encoded content", func(t *testing.T) {
-		params := url.Values{}
-		params.Set("content", "cellar")
-		encodedParams := strings.NewReader(params.Encode())
-
-		req, _ := http.NewRequest("POST", reqPath, encodedParams)
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-		recorder := httptest.NewRecorder()
-		router.ServeHTTP(recorder, req)
-
-		if recorder.Code != http.StatusBadRequest {
-			t.Errorf("Exepected status code 400; got %d", recorder.Code)
-		}
-
-		errResp := decodeError(recorder.Body.String())
-
-		if errResp.Message != "content must be base64 encoded" {
-			t.Errorf("Mismatched error message: %s", errResp.Message)
 		}
 	})
 }
